edd2270f6f8de381341ae7f39cb7388f1dece28c
Anshuman Solanki <anshumanreal399@gmail.com>
2020-09-29 17:12:10 +0530

    Feat:  -New method to access context  -It is applicable for both class and functional components, the previous one was only applicable to class components

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,25 +2,25 @@ import React, {Component} from 'react';
 import {Themes} from "../contexts/Themes";
 
 class NavigationBar extends Component {
-    static contextType = Themes;
     render(){
-        console.log(this.context);
-        const {isLightTheme, lightProps, darkProps}
-                                            = this.context;
-        const chosenTheme = isLightTheme ? lightProps : darkProps;
         return(
-            <nav style={{color : chosenTheme.textColor,
-                background : chosenTheme.componentColor }}>
-                <h1>Context-hooks-app</h1>
-                <p>Learn how to use context-hooks together for
-                    state management.</p>
-                <ul>
-                    <li>Home</li>
-                    <li>Books</li>
-                    <li>Contact</li>
-                </ul>
-
-            </nav>
+            <Themes.Consumer>{(context) => {
+                const {isLightTheme, lightProps, darkProps}
+                    = context;
+                const chosenTheme = isLightTheme ? lightProps : darkProps;
+                return(<nav style={{color : chosenTheme.textColor,
+                    background : chosenTheme.componentColor }}>
+                    <h1>Context-hooks-app</h1>
+                    <p>Learn how to use context-hooks together for
+                        state management.</p>
+                    <ul>
+                        <li>Home</li>
+                        <li>Books</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+                )
+            }}</Themes.Consumer>
         );
     }
 }
