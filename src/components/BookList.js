15336d327e83a0ae8a41e07694ad6258d4e8e6d7
Anshuman Solanki <anshumanreal399@gmail.com>
2020-09-30 18:40:27 +0530

    Learn:   -functional components using useContext hook

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,27 +1,23 @@
-import React, {Component} from 'react';
+import React, {useContext } from 'react';
 import {Themes} from "../contexts/Themes";
 
-class BookList extends Component {
-    static contextType = Themes;
-    render(){
-        const {isLightTheme, lightProps, darkProps}
-            = this.context;
-        const chosenTheme = isLightTheme ? lightProps : darkProps;
-        return(
-            <div className="bookList"
-                style={{color : chosenTheme.textColor,
-                    background : chosenTheme.background}}>
-                <ul>
-                    <li style={{background : chosenTheme.componentColor}}>
-                        Chacha Chaudhary</li>
-                    <li style={{background : chosenTheme.componentColor}}>
-                        The Invisible Man</li>
-                    <li style={{background : chosenTheme.componentColor}}>
-                        Et te brute!</li>
-                </ul>
-            </div>
-        );
-    }
+const BookList = () => {
+    const {isLightTheme, lightProps, darkProps} = useContext(Themes);
+    const chosenTheme = isLightTheme ? lightProps : darkProps;
+    return(
+        <div className="bookList"
+             style={{color : chosenTheme.textColor,
+                 background : chosenTheme.background}}>
+            <ul>
+                <li style={{background : chosenTheme.componentColor}}>
+                    Chacha Chaudhary</li>
+                <li style={{background : chosenTheme.componentColor}}>
+                    The Invisible Man</li>
+                <li style={{background : chosenTheme.componentColor}}>
+                    Et te brute!</li>
+            </ul>
+        </div>
+    )
 }
 
 export default BookList;
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,37 +1,28 @@
-import React, {Component} from 'react';
+import React, { useContext} from 'react';
 import {Themes} from "../contexts/Themes";
 import {AuthContext} from "../contexts/AuthContext";
 
-class NavigationBar extends Component {
-    render(){
-        return(
-            <AuthContext.Consumer>{ (authContext) => (
-                <Themes.Consumer>{(themeContext) => {
-                    const {isAuthenticated, toggleAuth} = authContext;
-                    console.log(toggleAuth);
-                    const {isLightTheme, lightProps, darkProps}
-                        = themeContext;
-                    const chosenTheme = isLightTheme ? lightProps : darkProps;
-                    return(
-                        <nav style={{color : chosenTheme.textColor,
-                            background : chosenTheme.componentColor }}>
-                            <h1>Context-hooks-app</h1>
-                            <p>Learn how to use context-hooks together for
-                                state management.</p>
-                            <div onClick={toggleAuth}>
-                                {isAuthenticated ? 'Logged In' : 'Logged Out'}
-                            </div>
-                            <ul>
-                                <li>Home</li>
-                                <li>Books</li>
-                                <li>Contact</li>
-                            </ul>
-                        </nav>
-                    )
-                }}</Themes.Consumer>
-            )}</AuthContext.Consumer>
-        );
-    }
+const NavigationBar = () => {
+    const {isAuthenticated, toggleAuth} = useContext(AuthContext);
+    const {isLightTheme, lightProps, darkProps}
+        = useContext(Themes);
+    const chosenTheme = isLightTheme ? lightProps : darkProps;
+    return(
+        <nav style={{color : chosenTheme.textColor,
+            background : chosenTheme.componentColor }}>
+            <h1>Context-hooks-app</h1>
+            <p>Learn how to use context-hooks together for
+                state management.</p>
+            <div onClick={toggleAuth}>
+                {isAuthenticated ? 'Logged In' : 'Logged Out'}
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Books</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
 }
 
 export default NavigationBar;
